3b720f30f73a798920901d3b019b7231deb82644
joshpled <joshuapleduc@gmail.com>
2020-10-28 12:25:25 -0400

    add communities to DOM

--- communities_frontend/src/communities.js
+++ communities_frontend/src/communities.js
@@ -7,13 +7,25 @@ class Community{
 function getAllCommunities(){
     fetch("http://localhost:3000/api/v1/communities")
     .then(resp => resp.json())
-    .then(data => createAllCommunities(data))
-}
-
-function createAllCommunities(arr){
-    return arr.map(function(x){
-        return x.name
+    .then(data =>{ 
+       let newArr = createAllCommunities(data)
+       displayCommunities(newArr)
     })
 }
 
+function createAllCommunities(arr){
+    let newArr = arr.map(function(x){
+        return x.name
+    })
+    return newArr
+}
 
+function displayCommunities(arr){
+    let ul = document.querySelector("#communitiesList")
+    for (let x of arr){
+        let li = document.createElement("li")
+        li.classname = "list-group-item"
+        li.innerHTML = x
+        ul.appendChild(li)
+    }
+}
