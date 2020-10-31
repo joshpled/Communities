d7097ac8be7960970652966f9cf26153ac28c4a3
joshpled <joshuapleduc@gmail.com>
2020-10-31 18:06:32 -0400

    cors origins update

--- communities_backend/config/initializers/cors.rb
+++ communities_backend/config/initializers/cors.rb
@@ -8,7 +8,7 @@ Rails.application.config.action_controller.forgery_protection_origin_check = fal
  
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080'
+    origins 'localhost:3000'
     
     resource '*',
       headers: :any,
